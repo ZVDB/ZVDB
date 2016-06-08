462005d27d9d596c5531db5c15058aec5c7ac98c
R F I D E <RFIDE@users.noreply.github.com>
2016-06-08 12:46:29 +0530

    Initial commit
    build.sh, shell script to --/build/-- whole "ZVDB®" in one go.
    Usage: $ sh build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+# ================================ ZVDB® ===============================
+# A Zika Virus proteins and their associated functional genes database.
+#                             R F I D E®
+#       Rosalind Elsie Franklin Institute for Disease Eradication
+#      Rosalind Elsie Franklin Institute για την εξάλειψη της νόσου
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#  Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ======================================================================
