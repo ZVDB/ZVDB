e5715b652af0cfbf2a5b544ff064d6d5b3e7af8a
R F I D E <RFIDE@users.noreply.github.com>
2016-06-08 14:56:44 +0530

    Initial commit
    Author: Prabhat Kumar, http://prabhatkumar.org/.

--- pub/data/reporter.py
+++ pub/data/reporter.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ZVDB®     : A Zika Virus proteins and their associated functional genes database.
+# RFIDE®    : Rosalind Elsie Franklin Institute for Disease Eradication.
+# Author    : Prabhat Kumar [http://prabhatkumar.org/].
+# Copyright : Sequømics Corporation [http://sequomics.com/].
